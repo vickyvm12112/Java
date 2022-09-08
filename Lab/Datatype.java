23c45f512fd300d2f11a45306b41688757b50bb5
vignesh vicky <113010948+vickyvm12112@users.noreply.github.com>
2022-09-08 15:09:27 +0530

    Add files via upload

---
+++ Lab/Datatype.java
@@ -0,0 +1,31 @@
+
+
+class Datatype
+{ 
+    public static void main(String[] args )
+	{
+		String name="Vignesh M";
+		int age=22;
+		double num1=25.124567890;
+		float num=45;
+		boolean state=true;
+		{
+			if(state == true);
+		}
+		char first_letter ='A';
+		short var=2;
+		byte val =0;
+		long value=9626638182l;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(num1);
+		System.out.println(num);
+		System.out.println("state is true");
+		System.out.println (first_letter);
+		System.out.println(var);
+		System.out.println(val);
+		System.out.println(value);
+		
+	}
+	
+}
