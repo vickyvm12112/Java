9ce99c94f3984e77f20889198069a12d7c152f18
vignesh vicky <113010948+vickyvm12112@users.noreply.github.com>
2022-09-15 17:55:05 +0530

    Add files via upload

---
+++ Lab/Largest.java
@@ -0,0 +1,25 @@
+//Name: Vignesh.M
+//Batch:2576
+//largest of 3 number
+class Largest
+{
+	public static void main(String[] args)
+	{
+	int n1=10;
+	int n2=20;
+	int n3=30;
+	if (n1>n2&&n1>n3)
+	{
+		System.out.println("n1 is largest:"+n1);
+	}
+	else if(n2>n1&&n2>n3)
+	{
+		System.out.println("n2 is largest:"+n2);
+	}
+	else 
+	{
+		System.out.println("n3 is largest:"+n3);
+	}
+	}
+}	
+	
